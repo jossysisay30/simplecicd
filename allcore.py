021f69851b14bb64b85733283d1a5c7231d16c88
jossysisay30 <joosy.sisay@gmail.com>
2022-03-31 18:05:12 +0300

    e0/0 descraption change

--- allcore.py
+++ allcore.py
@@ -3,7 +3,7 @@ from nornir.core.task import Result, Task
 from nornir_jinja2.plugins.tasks import template_file
 from nornir_utils.plugins.functions import print_result
 from nornir_scrapli.tasks import send_configs
-import  ipdb
+#import  ipdb
 nr = InitNornir(config_file="config.yaml")
 def basicconf(task):
     r=task.run(task=template_file,template="inter.j2",path=f"templates/")
